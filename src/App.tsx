0d859a83ddff1a4acca82ec0431c21b894dc4094
Dakshal Shah <dakshal@users.noreply.github.com>
2025-06-01 10:15:26 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           <Route path="/about" element={<AboutPage />} />
           <Route path="/services" element={<ServicesPage />} />
           <Route path="/services/:serviceType" element={<ServiceDetailPage />} />
-          <Route path="/services/:segmentId" element={<MarketSegmentDetails />} />
+          <Route path="/markets/:segmentId" element={<MarketSegmentDetails />} />
           <Route path="/projects" element={<ProjectsPage />} />
           <Route path="/tax-credits" element={<TaxCreditsPage />} />
           <Route path="/sales-rep" element={<SalesRepPage />} />
