acbd627fa8cf89791ea26f79467ed51bb35acd73
Dakshal Shah <dakshal@users.noreply.github.com>
2025-05-31 23:47:43 -0400

    Updated AboutPage.tsx

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -14,7 +14,7 @@ const AboutPage: React.FC = () => {
    maryland: { top: '50%', left: '45%' },
    dc: { top: '65%', left: '48%' },
    newjersey: { top: '58%', left: '70%' },
-   delaware: { top: '68%', left: '60%' }
+   delaware: { top: '68%', left: '62%' }
  };
   
   // Combine territory data with map coordinates
