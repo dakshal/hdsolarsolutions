cedc1e70e01ada1c1385c8b2a0f2cbdb519aa596
Dakshal Shah <dakshal@users.noreply.github.com>
2025-10-25 00:47:06 +0000

    update src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom' // Make sure this is imported
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    {/* Add the basename prop here */}
-    <BrowserRouter basename="/hdsolarsolutions/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
-// import { StrictMode } from 'react';
-// import { createRoot } from 'react-dom/client';
-// import { BrowserRouter } from 'react-router-dom';
-// import App from './App.tsx';
-// import './index.css';
-
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </StrictMode>
-// );
